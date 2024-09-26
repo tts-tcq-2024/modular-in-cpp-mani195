039e9f628c127eed72b7bf481a876c2f521009f3
Manibharathi <58763956+mani195@users.noreply.github.com>
2024-09-26 09:06:25 +0530

    Update majorColour.h

--- majorColour.h
+++ majorColour.h
@@ -1,1 +1,27 @@
+#pragma once
+
+#include <iostream>
+#include <map>
+#include <string>
+
+class MajorColorHandler 
+{
+    public:
+        enum class Name : std::uint8_t 
+        {
+          WHITE = 0x00,
+          RED = 0x01,
+          BLACK = 0x02,
+          YELLOW = 0x03,
+          VIOLET = 0x04,
+        };
+    
+    static int getNumberOfColors();
+    static std::string getColorName(const Name colorName);
+    static std::uint8_t getColorCode(const Name colorName);
+    
+    private:
+      static const std::map<Name, std::uint8_t> majorColorCodes;
+      static const std::map<Name, std::string> majorColorNames;
+};
 
