502877e0775cd8a28a73d69a9fd3e28408b2bbd9
Manibharathi <58763956+mani195@users.noreply.github.com>
2024-09-26 09:07:00 +0530

    Update majorColour.cpp

--- majorColour.cpp
+++ majorColour.cpp
@@ -1,1 +1,43 @@
+#include "MajorColorHandler.h"
+
+const std::map<MajorColorHandler::Name, std::uint8_t> MajorColorHandler::majorColorCodes = {
+        {MajorColorHandler::Name::WHITE, 0x00},
+        {MajorColorHandler::Name::RED, 0x01},
+        {MajorColorHandler::Name::BLACK, 0x02},
+        {MajorColorHandler::Name::YELLOW, 0x03},
+        {MajorColorHandler::Name::VIOLET, 0x04},
+};
+
+const std::map<MajorColorHandler::Name, std::string> MajorColorHandler::majorColorNames = {
+        {MajorColorHandler::Name::WHITE, "White"},
+        {MajorColorHandler::Name::RED, "Red"},
+        {MajorColorHandler::Name::BLACK, "Black"},
+        {MajorColorHandler::Name::YELLOW, "Yellow"},
+        {MajorColorHandler::Name::VIOLET, "Violet"},
+};
+
+int MajorColorHandler::getNumberOfColors()
+{
+    return majorColorNames.size();
+}
+
+std::string MajorColorHandler::getColorName(const Name colorName)
+{
+  auto iterator = majorColorNames.find(colorName);
+  if (iterator != majorColorNames.end()) 
+  {
+    return iterator->second;
+  }
+  return "";
+}
+
+std::uint8_t MajorColorHandler::getColorCode(const Name colorName)
+{
+  auto iterator = majorColorCodes.find(colorName);
+  if (iterator != majorColorCodes.end()) 
+  {
+    return iterator->second;
+  }
+  return 0;
+}
 
